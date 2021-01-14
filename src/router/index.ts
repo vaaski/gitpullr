0fc32660b0f99e76b27bd40208c8c543c56eb1c2
vaaski <admin@vaa.ski>
2021-01-14 12:12:19 +0100

    formatting

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
+import Home from "../views/Home.vue"
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -16,11 +16,11 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-});
+})
 
-export default router;
+export default router
