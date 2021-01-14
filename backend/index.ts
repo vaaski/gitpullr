abd6ba6391c0b8edb5519ee1f62f993e257bf832
vaaski <admin@vaa.ski>
2021-01-14 21:55:21 +0100

    feat(backend): determine changed files

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,11 @@
+import { ReceivedHook } from "./types"
+
 import Koa from "koa"
 import Router from "@koa/router"
 import mount from "koa-mount"
 import bodyParser from "koa-bodyparser"
 import frontend from "./frontend"
+import { uniqueArray } from "./utils"
 
 const dev = process.env.NODE_ENV === "development"
 
@@ -16,7 +19,22 @@ router.get("/hook", ctx => {
   ctx.body = "hook"
 })
 router.post("/hook", ctx => {
-  console.log(ctx.request.body)
+  let hook = ctx.request.body as ReceivedHook
+
+  // handle application/x-www-form-urlencoded
+  if ("payload" in hook && typeof hook.payload === "string")
+    hook = JSON.parse(hook.payload)
+
+  let changedFiles = []
+
+  if (!hook.commits) return
+  for (const commit of hook.commits) {
+    changedFiles.push(...commit.added, ...commit.modified)
+  }
+  changedFiles = uniqueArray(changedFiles)
+
+  log("changedFiles", changedFiles)
+
   ctx.status = 200
 })
 
---
+++ backend/utils.ts
@@ -0,0 +1,1 @@
+export const uniqueArray = (arr: any[]) => [...new Set(arr)]
