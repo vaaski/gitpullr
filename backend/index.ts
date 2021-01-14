0e5e09f93760306a3a180390e4f0ac64916daba6
vaaski <admin@vaa.ski>
2021-01-14 20:23:28 +0100

    feat(backend): add koa-bodyparser

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,23 @@
 import Koa from "koa"
 import Router from "@koa/router"
 import mount from "koa-mount"
+import bodyParser from "koa-bodyparser"
 import frontend from "./frontend"
 
 const dev = process.env.NODE_ENV === "development"
 
 const app = new Koa()
+app.use(bodyParser())
 const router = new Router()
 
 const log = (...t: any[]) => console.log("\u001b[90m[backend]:\u001b[39m", ...t)
 
 router.get("/hook", ctx => {
   ctx.body = "hook"
+})
+router.post("/hook", ctx => {
+  console.log(ctx.request.body)
+  ctx.status = 200
 })
 
 if (!dev) app.use(mount("/ui", frontend))
