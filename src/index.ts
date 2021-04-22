8f0a047e5f8184c9e7e8084f144e35f3dac8e52e
vaaski <admin@vaa.ski>
2021-04-22 20:49:06 +0200

    working prototype

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,43 @@
+import type { Config } from "$/Config"
+import type { GithubHook } from "$/GithubHook"
+
+import debug from "debug"
 import { read } from "fs-jetpack"
 import { join } from "path"
-import type { Config } from "../types/config"
+import express from "express"
+import execa from "execa"
+
+const log = debug("gitpullr")
 
 const config: Config = read(join(__dirname, "../config.json"), "json")
 
-console.log(config)
+const server = express()
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+
+server.post(config.hookPath, async (req, res) => {
+  // TODO: verify secret https://github.com/vaaski/gitpullr/blob/8755aed244a4bc8dd96840bb36687448359bb844/backend/secret.ts
+
+  const body = req.body as GithubHook.PushHook
+  const project = config.projects.find(p => p.name === body.repository.full_name)
+  if (!project) return res.sendStatus(404)
+
+  const reg = new RegExp(project.filter)
+  if (reg.test(body.head_commit.message)) return res.status(200).send("filter flag detected")
+
+  log(`new request for project ${project.name}`)
+
+  for (const command of project.exec) {
+    await execa.command(command, {
+      cwd: project.path,
+      stdout: process.stdout,
+      stderr: process.stderr,
+    })
+  }
+
+  res.sendStatus(200)
+})
+
+server.listen(config.hookPort, () => {
+  console.log(`gitpullr running on port ${config.hookPort} and path ${config.hookPath}`)
+})
--- types/config.d.ts
+++ types/config.d.ts
@@ -1,18 +1,13 @@
 export interface Config {
   hookPath: string
+  hookPort: number
   projects: Project[]
-}
-
-export type HookTypes = "push"
-
-export interface Hook {
-  exec: string
-  filter: string
 }
 
 export interface Project {
   name: string
   path: string
   secret: string
-  hooks: Record<HookTypes, Hook>
+  exec: string[]
+  filter: string
 }
